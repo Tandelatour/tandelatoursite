e14a2dfc4d10a828ab26afb731a7b1685febb306
alatour2022 <tandelatour@gmail.com>
2023-03-20 14:06:54 +0100

    Maj 1

---
+++ src/Controller/MainController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    #[Route('/', name: 'accueil')]
+    public function accueil(): Response
+    {
+        return $this->render('main/accueil.html.twig', [ ]);
+    }
+
+    #[Route('/main', name: 'app_main')]
+    public function index(): Response
+    {
+        return $this->render('main/index.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+
+
+
+
+
+}
